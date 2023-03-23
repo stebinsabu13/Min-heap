fe102ace7021bd2216d5a0d8708d08450dd6fe62
stebinsabu13 <60867706+stebinsabu13@users.noreply.github.com>
2023-03-23 09:53:59 +0530

    Add files via upload

---
+++ MinHeap.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type MinHeap struct {
+	arr []int
+}
+
+func (h *MinHeap) insert(val int) {
+	h.arr = append(h.arr, val)
+	h.heapifyTop()
+}
+func (h *MinHeap) extract() {
+	element := h.arr[0]
+	h.arr[0] = h.arr[len(h.arr)-1]
+	h.arr = h.arr[:len(h.arr)-1]
+	fmt.Println(element)
+	h.heapifyBottom()
+}
+func (h *MinHeap) heapifyBottom() {
+	index := 0
+	for lchild(index) <= len(h.arr)-1 {
+		lc := lchild(index)
+		rc := rchild(index)
+		if rc > len(h.arr)-1 && h.arr[lc] < h.arr[index] {
+			h.swap(lc, index)
+			index = lchild(index)
+		} else if h.arr[lc] < h.arr[rc] && h.arr[index] > h.arr[lc] {
+			h.swap(lc, index)
+			index = lchild(index)
+		} else if h.arr[rc] < h.arr[lc] && h.arr[index] > h.arr[rc] {
+			h.swap(rc, index)
+			index = rchild(index)
+		} else {
+			return
+		}
+	}
+}
+func (h *MinHeap) heapifyTop() {
+	index := len(h.arr) - 1
+	for h.arr[parent(index)] > h.arr[index] {
+		h.swap(parent(index), index)
+		index = parent(index)
+	}
+}
+func (h *MinHeap) swap(i, j int) {
+	h.arr[i], h.arr[j] = h.arr[j], h.arr[i]
+}
+func parent(i int) int {
+	return (i - 1) / 2
+}
+func lchild(i int) int {
+	return (i * 2) + 1
+}
+func rchild(i int) int {
+	return (i * 2) + 2
+}
+func main() {
+	var h MinHeap
+	arr := []int{1, 2, 3}
+	for _, v := range arr {
+		h.insert(v)
+	}
+	fmt.Println(h.arr)
+	h.extract()
+	fmt.Println(h.arr)
+}
